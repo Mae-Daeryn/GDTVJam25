a5d4262dcd60d519c4c895b899aff028b3d1435c
0xffff00 <juldre6@gmail.com>
2025-05-18 16:59:27 +0200

    Changed list to checkbox-list

--- GDTVJam25/Assets/Scripts/Habitat.cs
+++ GDTVJam25/Assets/Scripts/Habitat.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Habitat : MonoBehaviour
 {
-    [SerializeField]
-    public TerrariumType[] type;
+
+    public bool Rainforest;
+    public bool Desert;
+    public bool Water;
+
+    public TerrariumType[] ToTypes()
+    {
+        var types = new List<TerrariumType>();
+        if (Rainforest) types.Add(TerrariumType.Rainforest);
+        if (Desert) types.Add(TerrariumType.Desert);
+        if (Water) types.Add(TerrariumType.Water);
+        return types.ToArray();
+    }
+
 }
