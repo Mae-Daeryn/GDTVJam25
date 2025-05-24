9e7cdc19adcffb3f250a1d5cdd7c7056c6e5ad3f
0xffff00 <juldre6@gmail.com>
2025-05-24 15:17:32 +0200

    Moved score logic to UpdateScore component

--- GDTVJam25/Assets/Scripts/deko/SubmitJob.cs
+++ GDTVJam25/Assets/Scripts/deko/SubmitJob.cs
@@ -17,37 +17,38 @@ public class SubmitJob : MonoBehaviour
 
     void ButtonClicked()
     {
-        if (TaskDatabase.currentTask == null)
-        {
-            // Hier Panel oder so zum bescheid geben öffnen, dass der User noch kein Task ausgewählt hat
-            return;
-        }
-        var target = TaskDatabase.currentTask.type;
+        Debug.Log($"{ScoreManager.score}");
+        // if (TaskDatabase.currentTask == null)
+        // {
+        //     // Hier Panel oder so zum bescheid geben öffnen, dass der User noch kein Task ausgewählt hat
+        //     return;
+        // }
+        // var target = TaskDatabase.currentTask.type;
 
-        long sum = 0;
-        for (int i = 0; i < parentObject.transform.childCount; i++)
-        {
-            GameObject child = parentObject.transform.GetChild(i).gameObject;
-            if (excludeObjects.Contains(child)) continue;
-            Habitat h = child.GetComponent<Habitat>();
-            sum += h.GetScore(target);
-        }
-        if (TerraPanel.CompareTag(target.ToString()))
-        {
+        // long sum = 0;
+        // for (int i = 0; i < parentObject.transform.childCount; i++)
+        // {
+        //     GameObject child = parentObject.transform.GetChild(i).gameObject;
+        //     if (excludeObjects.Contains(child)) continue;
+        //     Habitat h = child.GetComponent<Habitat>();
+        //     sum += h.GetScore(target);
+        // }
+        // if (TerraPanel.CompareTag(target.ToString()))
+        // {
 
-            sum *= 2;
-        }
-        else
-        {
-            sum /= 2;
-        }
-        MoneyManager.Money += 100;
-        if (sum > 100)
-        {
-            MoneyManager.Money += 50;
-        }
-        Debug.Log(TerraPanel.tag);
-        Debug.Log(target.ToString());
-        Debug.Log(sum);
+        //     sum *= 2;
+        // }
+        // else
+        // {
+        //     sum /= 2;
+        // }
+        // MoneyManager.Money += 100;
+        // if (sum > 100)
+        // {
+        //     MoneyManager.Money += 50;
+        // }
+        // Debug.Log(TerraPanel.tag);
+        // Debug.Log(target.ToString());
+        // Debug.Log(sum);
     }
 }
