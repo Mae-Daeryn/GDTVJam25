f83253f8631094c6726087f33a00aaa26c3dbe73
0xffff00 <juldre6@gmail.com>
2025-05-24 15:46:57 +0200

    Added submit functionality

--- GDTVJam25/Assets/Scripts/deko/SubmitJob.cs
+++ GDTVJam25/Assets/Scripts/deko/SubmitJob.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SubmitJob : MonoBehaviour
@@ -9,7 +10,6 @@ public class SubmitJob : MonoBehaviour
     public Button button;
     public GameObject parentObject;
     public GameObject[] excludeObjects;
-    public GameObject TerraPanel;
     void Start()
     {
         button.onClick.AddListener(ButtonClicked);
@@ -17,38 +17,29 @@ public class SubmitJob : MonoBehaviour
 
     void ButtonClicked()
     {
+
+        var task = TaskDatabase.currentTask;
+        if (task == null)
+        {
+            // Panel: Kein auftrag angenommen
+            return;
+        }
+        if (ScoreManager.score < task.requiredScore)
+        {
+            // Panel: Score nicht hoch genug
+            return;
+        }
+        for (int i = 0; i < parentObject.transform.childCount; i++)
+        {
+            GameObject child = parentObject.transform.GetChild(i).gameObject;
+            if (excludeObjects.Contains(child)) continue;
+            DragDrop dd = child.GetComponent<DragDrop>();
+            MoneyManager.Money += dd.costs;
+            Destroy(child);
+        }
+        MoneyManager.Money += 100;
+        SceneManager.LoadScene(3);
+        // Panel: Abgegeben
         Debug.Log($"{ScoreManager.score}");
-        // if (TaskDatabase.currentTask == null)
-        // {
-        //     // Hier Panel oder so zum bescheid geben öffnen, dass der User noch kein Task ausgewählt hat
-        //     return;
-        // }
-        // var target = TaskDatabase.currentTask.type;
-
-        // long sum = 0;
-        // for (int i = 0; i < parentObject.transform.childCount; i++)
-        // {
-        //     GameObject child = parentObject.transform.GetChild(i).gameObject;
-        //     if (excludeObjects.Contains(child)) continue;
-        //     Habitat h = child.GetComponent<Habitat>();
-        //     sum += h.GetScore(target);
-        // }
-        // if (TerraPanel.CompareTag(target.ToString()))
-        // {
-
-        //     sum *= 2;
-        // }
-        // else
-        // {
-        //     sum /= 2;
-        // }
-        // MoneyManager.Money += 100;
-        // if (sum > 100)
-        // {
-        //     MoneyManager.Money += 50;
-        // }
-        // Debug.Log(TerraPanel.tag);
-        // Debug.Log(target.ToString());
-        // Debug.Log(sum);
     }
 }
