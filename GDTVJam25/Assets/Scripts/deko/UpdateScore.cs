5583a8653d28c324fa4e41c41d0bdf8257d46242
0xffff00 <juldre6@gmail.com>
2025-05-24 15:17:15 +0200

    Added UpdateScore component

---
+++ GDTVJam25/Assets/Scripts/deko/UpdateScore.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class UpdateScore : MonoBehaviour
+{
+    public GameObject parentObject;
+    public GameObject[] excludeObjects;
+    public GameObject TerraPanel;
+
+    public TextMeshProUGUI scoreObject;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TaskDatabase.currentTask == null)
+        {
+            scoreObject.text = "0";
+            return;
+        }
+        var target = TaskDatabase.currentTask.type;
+        long sum = 0;
+        for (int i = 0; i < parentObject.transform.childCount; i++)
+        {
+            GameObject child = parentObject.transform.GetChild(i).gameObject;
+            if (excludeObjects.Contains(child)) continue;
+            Habitat h = child.GetComponent<Habitat>();
+            sum += h.GetScore(target);
+        }
+        if (TerraPanel.CompareTag(target.ToString()))
+        {
+
+            sum *= 2;
+        }
+        else
+        {
+            sum /= 2;
+        }
+        scoreObject.text = $"{sum}";
+    }
+}
