c8895788c257ab4bf4e626eb5f1cb0a8b96a4dee
HabEinKeks <habeinkeks@gmail.com>
2025-05-26 17:14:43 +0200

    added background music
    added background music

---
+++ GDTVJam25/Assets/Scripts/Raumansicht/music.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class music : MonoBehaviour 
+{
+
+    public static music instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
