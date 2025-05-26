4c964570f33a65408ce103ededd519fabb8a9e2e
HabEinKeks <habeinkeks@gmail.com>
2025-05-26 17:52:47 +0200

    added quests and JP Font
    added quests and JP Font

--- GDTVJam25/Assets/Scripts/Computer/TaskDatabase.cs
+++ GDTVJam25/Assets/Scripts/Computer/TaskDatabase.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 public static class TaskDatabase
@@ -8,170 +8,114 @@ public static class TaskDatabase
     {
         new Task
         {
-            subject = "Bearded Dragon Terra build",
-            sender = "Margaret",
-            sendermail = "noIdeaWhatToPutHere@gmail.com",
-            mailContent = "Hiii,\n" +
-            "I need a Tank for a Bearded Dragon.\n" +
-            "Thanks,\n" +
-            "Margaret",
-            description = "Build a Desert Terrarium for a Bearded Dragon",
+            subject = "Betta Fish Tank",
+            sender = "Kungfu Sandra",
+            sendermail = "Kungfu Sandra",
+            mailContent = @"Dear Terra-Building,
+
+my name is Sandra and I always dreamed of owning my own Fish Tank! HIYAHHH! Of course I love Betta fish the most. My Betta is called Goku and he cost over 9000 $! He should be in the spotlight. 
+I want it to look like an arena or something like that! Can you do that?!
+Alright! Fist on it!
+
+Kungfu Sandra",
+            description = "Build a Betta fish tank for little Goku",
+            type = DragDrop.terraType.water,
+            requiredScore = 25
+        },
+        new Task
+        {
+            subject = "Day Gecko!!!!",
+            sender = "Kenny",
+            sendermail = "TheSurvivor.Kenny06@weirddomain.com",
+            mailContent = @"Terra-Build! It's me! It's Kenny!!
+
+I've got an super important request for you!
+Please make me a terrarium for my Day Gecko (she seems to be shy, but I'm 101% sure that she's just pretending to be shy).
+It should be awesome to have different layers of plants which provide enough cover (she's watching me sometimes. I don't know why. Maybe she's some kind of supernatural being?!).
+However, everything is fine (No it's not! Please make it a steady terrarium)!
+
+Despaired regards from KENNY!!!!",
+            description = "Rainforest ambience with lots of humidity and plants",
+            type = DragDrop.terraType.rainforest,
+            requiredScore = 25
+        },
+        new Task
+        {
+            subject = "Tree Frogs are sooo sweeet!",
+            sender = "Mary",
+            sendermail = "kawaii.uwu@love2u.com",
+            mailContent = @"Hiii~ hiii~, Terra-Build!
+
+My name is Mary and I loooove frogs!
+Omg, they're so adorable! <3
+I mean, look at their cutie cute blobby eyes and their smol wiggly butty butt! UwU
+I always wanted to have a super sweet and colourful terrarium with lots of froggos in it! And then I saw that advertisement of yours. FR, I was like: O M G! That's exactly what I need! Will you make a pretty home for my beloved Tree Frogs pleeeaaaase?
+
+Lovely regards,
+Mary ^-^",
+            description = "Rainforest ambience with lots of humidity and plants",
+            type = DragDrop.terraType.rainforest,
+            requiredScore = 25
+        },
+        new Task
+        {
+            subject = "Chameleon Terrarium",
+            sender = "Jody",
+            sendermail = "name.anothername99@anonymous.net",
+            mailContent = @"Hello.
+
+I've got a Chameleon. I only care for a good environment within the terrarium. I don't care what it looks like as long as the Chameleon will feel safe and well. That's it.
+
+Jody",
+            description = "Rainforest ambience with lots of humidity and plants",
+            type = DragDrop.terraType.rainforest,
+            requiredScore = 25
+        },
+        new Task
+        {
+            subject = "Chillin cool terra",
+            sender = "Diego",
+            sendermail = "manly.man@coolcaps.nice",
+            mailContent = @"Yoo, wazzup Daeryns!
+
+The name's Diego. I've got the coolest Sand Boa around. Her name is Buffy. Check zis pic out: [snake.png.fileerror]
+Anyways, I'm too cool for random stuff. Make it look gangster-like! Skulls, bones or dangerous looking plants and so on. You got me?
+
+Cya bro!
+",
+            description = "Rainforest ambience with lots of humidity and plants",
+            type = DragDrop.terraType.rainforest,
+            requiredScore = 25
+        },
+        new Task
+        {
+            subject = "フトアゴヒゲトカゲ",
+            sender = "正田崇弘義",
+            sendermail = "正田崇.弘義@ケーキ国.jp",
+            mailContent = @"こんにちは、親愛なる会社様。アゴヒゲトカゲ用のテラリウムが必要です。見た目をきれいに整えてもらえますか？予算は関係ありません。      どうもありがとう。
+心から、
+正田崇弘義
+
+Translation by noodle translate:
+Good day, fighting office. I need a terrarium for a beard in a game. Please make it look correct and visible. Many coins don't play a role. Thank you!
+Until we meet again,
+Name",
+            description = "Not sure what the customer wants... Just build a desert Terrarium",
             type = DragDrop.terraType.desert,
             requiredScore = 25
         },
         new Task
         {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
-            type = DragDrop.terraType.rainforest,
-            requiredScore = 25
-        },
-        new Task
-        {
-            subject = "Frog-Terrarium",
-            sender = "froglover69",
-            sendermail = "froglover69@froggystyle.de",
-            mailContent = "Dear Terra-Building,\n" +
-            "pls make me a Frog Tank.\n" +
-            "Best Regards,\n" +
-            "FrogLover from FroggyStyle",
-            description = "Rainforest ambience with lots of humidity and plants",
+            subject = "Job",
+            sender = "Sir Brown",
+            sendermail = "sirbrown.important@rich.money",
+            mailContent = @"Greetings, fellow workers of the adressed company.
+
+I am Sir Brown of Whippersnapper, an extremely rich and important person. I shall grant you the privilege to commit a design for my bejewled terrarium. My most honored Desert Tortoise, Sir Toir le Toise, demands an adequate interior design. If thy shall succeed on this noble quest of mine, wealth will be upon you.
+Wealthy regards,
+
+Sir Brown of Whippersnapper",
+            description = "Build a Desert Terrarium for the Tortoise 'Sir Toir le Toise' from Sir Brown",
             type = DragDrop.terraType.rainforest,
             requiredScore = 25
         }
